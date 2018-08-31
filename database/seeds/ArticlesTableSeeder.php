aab61b26992e2ec9147b68d9ba865d9db6e0775f
ThetMinnHtun <thetminnhtun92@gmail.com>
2018-08-31 22:39:35 +0630

    Articles CRUD complete

--- database/seeds/ArticlesTableSeeder.php
+++ database/seeds/ArticlesTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Article;
 use Illuminate\Database\Seeder;
 
 class ArticlesTableSeeder extends Seeder
@@ -11,6 +12,17 @@ class ArticlesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+         // Let's truncate our existing records to start from scratch.
+        Article::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        // And now, let's create a few articles in our database:
+        for ($i = 0; $i < 50; $i++) {
+            Article::create([
+                'title' => $faker->sentence,
+                'body' => $faker->paragraph,
+            ]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+Use App\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,8 +18,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/', function() {
-	$faker = \Faker\Factory::create();
-
-	return $faker->name;
-});
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@destroy');
